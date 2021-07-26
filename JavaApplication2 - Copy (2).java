f44c6bcb88ab8dec4db417ca7308706fc17a6fe8
AC-Ashqar <86671154+AC-Ashqar@users.noreply.github.com>
2021-07-26 12:31:35 +0300

    Update JavaApplication2 - Copy (2).java

--- JavaApplication2 - Copy (2).java
+++ JavaApplication2 - Copy (2).java
@@ -14,7 +14,7 @@ public class JavaApplication2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {dddddd
         // TODO code application logic here
     }
     
