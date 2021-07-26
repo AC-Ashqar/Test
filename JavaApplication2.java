cecff960547c925c853f73bb4928e56ed5046d54
AC-Ashqar <86671154+AC-Ashqar@users.noreply.github.com>
2021-07-26 12:32:36 +0300

    Update JavaApplication2.java

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -14,9 +14,7 @@ public class JavaApplication2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {sssss
-        // TODO code application logic heressss
-		sout("ssssssss");
+
     }
     
 }
