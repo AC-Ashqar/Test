2ae105540c00e0071c7b2cfdc45cb76be19a8bc2
AC-Ashqar <86671154+AC-Ashqar@users.noreply.github.com>
2021-07-27 09:42:15 +0300

    wwww

---
+++ B/JavaApplication2.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {ssssws
+        // TODO code application logic here
+desktop
+desktopaaaa
+desktop
+desktop
+desktop
+desktop
+desktop
+desktopssss
+desktopssss
+desktop
+desktop
+web
+web
+web
+web
+    web    
+        web
+        
+        web
+
+		desktop
+desktop
+desktop
+desktop
+desktop
+desktop
+desktop
+desktop
+desktop
+desktop
+desktop
+
+
+    public static void main(String[] args) {sssss
+        // TODO code application logic heressss
+		sout("ssssssss");
+
+    }
+    sssss
+}
